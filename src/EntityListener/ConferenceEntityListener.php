ae73f5bd9622c5ed3dc77adafa83661323b26745
medukr <andrii.demydyuk@gmail.com>
2020-07-25 23:44:59 +0300

    13.4

---
+++ src/EntityListener/ConferenceEntityListener.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by andrii
+ * Date: 25.07.20
+ * Time: 23:37
+ */
+
+namespace App\EntityListener;
+
+use App\Entity\Conference;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class ConferenceEntityListener
+{
+    private $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    public function prePersist(Conference $conference, LifecycleEventArgs $event)
+    {
+        $conference->computeSlug($this->slugger);
+    }
+
+    public function preUpdate(Conference $conference, LifecycleEventArgs $event)
+    {
+        $conference->computeSlug($this->slugger);
+    }
+}
